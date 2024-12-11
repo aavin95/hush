e98df44ec640152068f38823597337506898de0c
aavin@umich.edu <aavin@umich.edu>
2024-12-11 16:29:28 -0500

    user creation is working with supabase backend, had to fix .env issues.

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,17 @@
-// lib/supabaseClient.ts
-
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
+import path from "path";
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-export const supabase = createClient(supabaseUrl, supabaseKey);
+dotenv.config({ path: path.join(process.cwd(), "../.env") });
+
+if (!process.env.NEXT_PUBLIC_SUPABASE_URL) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_URL");
+}
+if (!process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error("Missing env.NEXT_PUBLIC_SUPABASE_ANON_KEY");
+}
+
+export const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
